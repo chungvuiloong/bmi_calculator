638b4bda6089d2263e6ac4d1664b3fd7e68cd835
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-12 07:26:40 +0300

    Created Benefits section

---
+++ src/component/Benefits/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const data =[
+    {
+        title: 'Healthy eating',
+        description: 'Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.'
+    },
+    {
+        title: 'Regular exercise',
+        description: 'Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.'
+    },
+    {
+        title: 'Adequate sleep',
+        description: 'Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.'
+    }
+]
+const index = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default index;
