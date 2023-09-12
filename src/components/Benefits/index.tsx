cc8314092608300460c98afe9a5bf410630ff943
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-12 21:06:01 +0300

    Updated Benefits component with Image & icon.

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import icon_eating from '@/assets/images/icon-eating.svg'
+import icon_exercise from '@/assets/images/icon-exercise.svg'
+import icon_sleep from '@/assets/images/icon-sleep.svg'
+import Image from 'next/image';
 
 const data =[
     {
         title: 'Healthy eating',
-        description: 'Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.'
+        description: 'Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.',
+        icon: icon_eating
     },
     {
         title: 'Regular exercise',
-        description: 'Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.'
+        description: 'Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.',
+        icon: icon_exercise
     },
     {
         title: 'Adequate sleep',
-        description: 'Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.'
+        description: 'Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.',
+        icon:  icon_sleep
     }
 ]
-const index = () => {
+const Benefits = () => {
     return (
         <div>
-            
+            <Image
+                // src={data[1].icon}
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                />
         </div>
     );
 };
 
-export default index;
+export default Benefits;
