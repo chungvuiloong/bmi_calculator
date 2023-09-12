6408208f571ebe4fd9900f557e1bb476ef1906f1
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-12 07:17:53 +0300

    Created herobanner

---
+++ src/component/HeroBanner/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ export  const HeroBanner = () => {
+    return (
+        <div>
+            <div className='container'>
+
+            </div>
+        </div>
+    );
+};
