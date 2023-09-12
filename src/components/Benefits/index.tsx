ed8b1dd4a5bb85aaa27fdc12214df3480972b970
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-12 21:25:56 +0300

    Add gap between title and description

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -30,7 +30,7 @@ const Benefits = () => {
                 {
                     benefits_data.map((data, index)=>
                         <Card key={index} className='border-2'>
-                            <div className='flex flex-col'>                        
+                            <div className='flex flex-col gap-6'>                        
                                 <Typography variant='h3'>{data.title}</Typography>
                                 <Typography variant='paragraph'>{data.description}</Typography>
                             </div>
