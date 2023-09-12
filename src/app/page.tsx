7c9c4de72e0dcd4a4694e2bd3ad813201766be8d
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-12 07:18:06 +0300

    Imported herobanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import HeroBanner from '../component/HeroBanner'
+import { HeroBanner } from '../component/HeroBanner'
 
 export default function Home() {
   return (
