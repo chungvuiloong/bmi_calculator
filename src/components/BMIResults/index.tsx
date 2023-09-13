8c5b96f9a3c87d6266dd4dfe570cad99c1f082a8
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 10:14:24 +0300

    Imported Typography and replaced tags.

--- src/components/BMIResults/index.tsx
+++ src/components/BMIResults/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@material-tailwind/react';
 import React from 'react';
 
 export  const BMIResults = () => {
@@ -5,16 +6,18 @@ export  const BMIResults = () => {
         <section>
             <div className='container mx-auto'>            
                 <div>
-                    <h2>What your BMI result means</h2>
-                    <p>
-                    A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
-                    Maintaining a healthy weight may lower your chances of
-                    experiencing health issues later on, such as obesity and type
-                    2 diabetes. Aim for a nutritious diet with reduced fat and
-                    sugar content, incorporating ample fruits and vegetables.
-                    Additionally, strive for regular physical activity, ideally about
-                    30 minutes daily for five days a week.
-                    </p>
+                    <Typography variant='h2'>
+                        What your BMI result means
+                    </Typography>
+                    <Typography variant='paragraph'>                    
+                        A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
+                        Maintaining a healthy weight may lower your chances of
+                        experiencing health issues later on, such as obesity and type
+                        2 diabetes. Aim for a nutritious diet with reduced fat and
+                        sugar content, incorporating ample fruits and vegetables.
+                        Additionally, strive for regular physical activity, ideally about
+                        30 minutes daily for five days a week.
+                    </Typography>
                 </div>
             </div>
         </section>
