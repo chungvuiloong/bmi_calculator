2c6640a2832a57f820af09b91668f862c5ddadf7
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 21:40:47 +0300

    Used a new layout.

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 
  export  const HeroBanner = () => {
     return (
-        <div>
-            <div className='container'>
-                <div>
-                    <h1>Body Mass Index Calculator</h1>
-					<p>
-						Better understand your weight in relation to your height using
-						our body mass index (BM) calculator. While BMI is not the
-						sole determinant of a healthy weight, it offers a valuable
-						starting point to evaluate your overall health and well-being.
-					</p>
+        <section>
+            <div className='py-14 bg-[#d8e7fe] lg:rounded-r-3xl rounded-none'>
+                <div className='px-5 md:px-8'>
+                    <div className='container mx-auto flex'>   
+                        <div className='basis-1/2'>
+                            <h1>Body Mass Index Calculator</h1>
+                            <p>
+                                Better understand your weight in relation to your height using
+                                our body mass index (BM) calculator. While BMI is not the
+                                sole determinant of a healthy weight, it offers a valuable
+                                starting point to evaluate your overall health and well-being.
+                            </p>
+                        </div>
+
+                    </div>
                 </div>
-
-
             </div>
-        </div>
+        </section>
     );
 };
