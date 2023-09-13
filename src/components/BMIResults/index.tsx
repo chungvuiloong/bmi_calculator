204ddabfed7e91b61ee28efa2717a4a2a5a6ec25
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 21:35:31 +0300

    Used new layout.

--- src/components/BMIResults/index.tsx
+++ src/components/BMIResults/index.tsx
@@ -6,29 +6,31 @@ import React from 'react';
 
 export  const BMIResults = () => {
     return (
-        <section>
-            <div className='py-14 px-5 md:px-8 lg:container mx-auto'>
-                <div className='flex md:flex-row flex-col gap-20'>
-                    <div className='basis-1/2'>                    
-                        <Image 
-                            src={eating_image} 
-                            alt='Eating Image'
-                            className='m-w-[200px] h-fit'
-                        />   
-                    </div>
-                    <div className='basis-1/2 self-center'>
-                        <Typography variant='h2' className='font-semibold'>
-                            What your BMI result means
-                        </Typography>
-                        <Typography variant='paragraph'>                    
-                            A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
-                            Maintaining a healthy weight may lower your chances of
-                            experiencing health issues later on, such as obesity and type
-                            2 diabetes. Aim for a nutritious diet with reduced fat and
-                            sugar content, incorporating ample fruits and vegetables.
-                            Additionally, strive for regular physical activity, ideally about
-                            30 minutes daily for five days a week.
-                        </Typography>
+        <section className='py-14 '>
+            <div className='px-5 md:px-0 lg:px-8 md:pr-5 '>
+                <div className='container mx-auto'>                
+                    <div className='flex md:flex-row flex-col lg:gap-20 gap-12'>
+                        <div className='basis-1/2'>                    
+                            <Image 
+                                src={eating_image} 
+                                alt='Eating Image'
+                                className='ml-0 md:-ml-10 lg:ml-0 m-w-[200px] h-fit'
+                            />   
+                        </div>
+                        <div className='basis-1/2 self-center flex flex-col gap-8'>
+                            <Typography variant='h2' className='font-semibold'>
+                                What your BMI result means
+                            </Typography>
+                            <Typography variant='paragraph'>                    
+                                A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
+                                Maintaining a healthy weight may lower your chances of
+                                experiencing health issues later on, such as obesity and type
+                                2 diabetes. Aim for a nutritious diet with reduced fat and
+                                sugar content, incorporating ample fruits and vegetables.
+                                Additionally, strive for regular physical activity, ideally about
+                                30 minutes daily for five days a week.
+                            </Typography>
+                        </div>
                     </div>
                 </div>
             </div>
