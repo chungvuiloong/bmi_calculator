c1411fc0d15e69703354d662625bf5a0a3cf8559
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 21:58:05 +0300

    Imported Typography.

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@material-tailwind/react';
 import React from 'react';
 
  export  const HeroBanner = () => {
@@ -6,16 +7,15 @@ import React from 'react';
             <div className='py-14 bg-[#d8e7fe] lg:rounded-r-3xl rounded-none'>
                 <div className='px-5 md:px-8 xl:px-32 '>
                     <div className='container mx-auto flex'>   
-                        <div className='basis-1/2'>
-                            <h1>Body Mass Index Calculator</h1>
-                            <p>
+                        <div className='basis-1/2 flex flex-col gap-8'>
+                            <Typography variant='h1'>Body Mass Index Calculator</Typography>
+                            <Typography variant='paragraph'>
                                 Better understand your weight in relation to your height using
                                 our body mass index (BM) calculator. While BMI is not the
                                 sole determinant of a healthy weight, it offers a valuable
                                 starting point to evaluate your overall health and well-being.
-                            </p>
+                            </Typography>
                         </div>
-
                     </div>
                 </div>
             </div>
