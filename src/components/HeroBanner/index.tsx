ac321a39a109dea97a8e29a34a94bd9b957fea59
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 22:00:13 +0300

    Imported use client. Add breakline.

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -1,14 +1,15 @@
+'use client'
 import { Typography } from '@material-tailwind/react';
 import React from 'react';
 
- export  const HeroBanner = () => {
+export  const HeroBanner = () => {
     return (
         <section>
             <div className='py-14 bg-[#d8e7fe] lg:rounded-r-3xl rounded-none'>
                 <div className='px-5 md:px-8 xl:px-32 '>
                     <div className='container mx-auto flex'>   
                         <div className='basis-1/2 flex flex-col gap-8'>
-                            <Typography variant='h1'>Body Mass Index Calculator</Typography>
+                            <Typography variant='h1'>Body Mass<br/>Index Calculator</Typography>
                             <Typography variant='paragraph'>
                                 Better understand your weight in relation to your height using
                                 our body mass index (BM) calculator. While BMI is not the
