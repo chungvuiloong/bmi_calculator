1402933effd3292c037f04fa5c76aa87e6a3f996
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-13 10:12:06 +0300

    Changed layout and added container.

--- src/components/BMIResults/index.tsx
+++ src/components/BMIResults/index.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 export  const BMIResults = () => {
     return (
         <section>
-            <div>
-                <h2>What your BMI result means</h2>
-                <p>
-                A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
-                Maintaining a healthy weight may lower your chances of
-                experiencing health issues later on, such as obesity and type
-                2 diabetes. Aim for a nutritious diet with reduced fat and
-                sugar content, incorporating ample fruits and vegetables.
-                Additionally, strive for regular physical activity, ideally about
-                30 minutes daily for five days a week.
-                </p>
+            <div className='container mx-auto'>            
+                <div>
+                    <h2>What your BMI result means</h2>
+                    <p>
+                    A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos;
+                    Maintaining a healthy weight may lower your chances of
+                    experiencing health issues later on, such as obesity and type
+                    2 diabetes. Aim for a nutritious diet with reduced fat and
+                    sugar content, incorporating ample fruits and vegetables.
+                    Additionally, strive for regular physical activity, ideally about
+                    30 minutes daily for five days a week.
+                    </p>
+                </div>
             </div>
         </section>
     );
