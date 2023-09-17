3e3ce6995a04df157fa4d318691ed2d11ddabb8d
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-17 17:42:49 +0300

    Add shadow to card.

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 function Bmi_calculator () {
     return (
-        <div className='basis-1/2 flex flex-col bg-white rounded-2xl p-7 gap-6 '>
+        <div className='basis-1/2 flex flex-col bg-white rounded-2xl p-7 gap-6 shadow-xl'>
             <Typography variant='h3'>Enter your details below</Typography>
             <fieldset>
                 <legend className="sr-only">BMI Calculation</legend>
