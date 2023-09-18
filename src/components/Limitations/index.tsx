f2633f5b275f032a735cf6d163eb179c10cc22d3
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-18 20:16:31 +0300

    Added class to dataset.

--- src/components/Limitations/index.tsx
+++ src/components/Limitations/index.tsx
@@ -12,26 +12,31 @@ const limitation_card_data =  [
     {
         title: "Gender",
         icon: gender_icon,
+        class: "ml-6 col-start-4/5 col-span-2",
         description: "The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI."
     },
     {
         title: "Age",
         icon: age_icon,
+        class: "col-start-3 col-span-2",
         description: "In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content."
     },
     {
         title: "Muscle",
         icon: muscle_icon,
+        class: "col-start-5 col-span-2",
         description: "BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."
     },
     {
         title: "Pregnancy",
         icon: pregnancy_icon,
+        class: "col-start-2 col-span-2",
         description: "Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."
     },
     {
         title: "Race",
         icon: race_icon,
+        class: "col-start-4 col-span-2",
         description: "Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practical nurse."
     }
 ]
@@ -41,15 +46,37 @@ export const Limitation = () => {
         <section className='relative'>
             <div className='py-14'>
                 <div className='relative px-5 md:px-8 xl:px-32 z-10'>
-                    <div className='container mx-auto flex lg:flex-row flex-col gap-12 w-fit'>
-                        {/* <div className='grid grid-cols-6 gap-4'>
-                            <div className='col-start-1 col-span-3 bg-red-500'>Text </div>
-                            <div className='col-start-4 col-span-2 bg-blue-500'>First Test</div>
+                    <div className='container mx-auto'>
+                        <div className='grid grid-cols-6 gap-6'>
+                            <div className='mr-6 col-start-1 col-span-3 flex flex-col gap-4'>
+                                <Typography variant='h2' className='font-semibold'>Limitations of BMI</Typography>
+                                <Typography variant='paragraph'>
+                                    Although BMI is often a practical indicator of healthy weight, 
+                                    it is not suited for every person. Specific groups should carefully consider their BMI outcomes, 
+                                    and in certain cases, the measurement may not be beneficial to use.
+                                </Typography> 
+                            </div>
+                            {
+                                limitation_card_data.map((data, index) =>
+                                    <Card key={index} className={`${data.class} p-7 shadow-2xl flex gap-4`}>
+                                        <div className='flex flex-row gap-3'>
+                                            <Image src={data.icon} alt={`${data.title} icon`} height={30} width={30} />
+                                            <div className='flex flex-col justify-center'>
+                                                <Typography variant='h6'>{data.title}</Typography>
+                                            </div>
+                                        </div>
+                                        <Typography variant='paragraph'>
+                                            {data.description}
+                                        </Typography>
+                                    </Card>
+                                )
+                            }
+                            {/* <div className='col-start-4 col-span-2 bg-blue-500'>First Test</div>
                             <div className='col-start-3 col-span-2 bg-blue-700'>Second Test</div>
                             <div className='col-start-5 col-span-2 bg-gray-700'>Second Test</div>
                             <div className='col-start-2 col-span-2 bg-blue-700'>Second Test</div>
-                            <div className='col-start-4 col-span-2 bg-gray-700'>Second Test</div>
-                        </div>             */}
+                            <div className='col-start-4 col-span-2 bg-gray-700'>Second Test</div> */}
+                        </div>            
                         {/* <div className='basis-1/2 flex flex-col gap-4 h-fit'>          
                             <Typography variant='h2' className='font-semibold'>Limitations of BMI</Typography>
                             <Typography variant='paragraph'>
