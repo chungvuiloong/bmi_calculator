519d9e12d02bf9468c9327156e3297aca4739723
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-22 10:56:12 +0300

    Added imperial calculation

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,14 +1,12 @@
-export function calculateBMI (height: number, weight: number, system: string) {
-    if (system === "Metrics") {
-        return (weight / (( height / 100) * ( height / 100 ))).toFixed(2)
-    } else {
-        return ((weight / (height * height)) * 703).toFixed(2)
-    }  
+export function calculateBMI (height: number, weight: number) {    
+        return ( weight / (( height / 100) * ( height / 100 ))).toFixed(2)
 }
 
-export function checkBMICategory (height: number, weight: number) {
-    const bmi = weight / ((height / 100) * (height / 100));
+export function calculateImperialBMI (height: number, weight: number) {
+    return (( weight / (height * height)) * 703 ).toFixed(2)
+}
 
+export function checkBMICategory (bmi: number) {
     if (bmi < 18.5) {
         return "underweight";
     } else if (bmi >= 18.5 && bmi < 25) {
