a454ff1fd26503fbc993536d5f9f4b71604da726
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-09-22 09:21:01 +0300

    Added checkBMI category.

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,3 +1,17 @@
 export function calculateBMI (height: number, weight: number) {
     return (weight / (( height / 100) * ( height / 100 ))).toFixed(1)
 }
+
+export function checkBMICategory (height: number, weight: number) {
+    const bmi = weight / ((height / 100) * (height / 100));
+
+    if (bmi < 18.5) {
+        <>are{" "}<span className="font-semibold">underweight</span></>;
+    } else if (bmi < 25) {
+        return <>have a{" "}<span className="font-semibold">healthy weight</span></>;
+    } else if (bmi < 30) {
+        return <>are{" "}<span className="font-semibold">overweight</span></>;
+    } else {
+        return <>are{" "}<span className="font-semibold">obese</span></>;
+    }
+}
